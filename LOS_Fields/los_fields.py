093304214d85dbdac8aba5c3057c09a662e426ae
Daniel B <db737@tutanota.com>
2020-11-11 21:49:05 +0000

    change H I to O I and add new plot

--- LOS_Fields/los_fields.py
+++ LOS_Fields/los_fields.py
@@ -3,6 +3,7 @@ import numpy as np
 import scipy.integrate as si
 import scipy.special as ss
 import matplotlib.pyplot as plt
+import matplotlib.lines as ml
 import math
 import sys
 
@@ -16,18 +17,21 @@ om_m = 0.308
 om_La = 0.692
 # Hubble constant, in SI units (s^{-1})
 H_0 = 2.1972e-18
-# Lyman-alpha transition frequency (Hz)
-nu_12 = 2.4661e15
-# Lyman-alpha damping constant
-Ga = 6.265e8
-# Proton mass (kg)
-m_p = 1.6726e-27
-# Boltmann constant (JK^{-1})
+# Transition frequency (Hz)
+# (https://physics.nist.gov/PhysRefData/ASD/lines_form.html) [NIST]
+nu_12 = 2.3023e15
+# Quantum-mechanical damping constant [NIST]
+Ga = 3.41e8
+# Particle mass (kg)
+m_p = 2.6788e-26
+# Boltzmann constant (JK^{-1})
 k_B = 1.3806e-23
 # Speed of light (ms^{-1})
 c = 2.9979e8
-# Prefactor I_\alpha as given in Choudhury et al. (2001) [C2001], (m^2)
-I_al = 4.45e-22
+# Prefactor I_\alpha to the integral, calculated using above constants and
+# https://www.astro.ncu.edu.tw/~wchen/Courses/ISM/04.EinsteinCoefficients.pdf
+# TODO: find more accurate OI value
+I_al = 1.971e-22
 # \sqrt{\pi}
 sqrt_pi = math.sqrt(pi)
 
@@ -41,8 +45,8 @@ nHIss = np.loadtxt("Input_1_nHI_Field.txt") * 1.0e6
 Tss = np.loadtxt("Input_2_Temperature_Field.txt")
 vss = np.loadtxt("Input_3_Line_of_Sight_Velocity_Field.txt") * 1000.0
 
-# Convert temperature to b as defined in [C2001], equation 31, for the nth
-# sightline
+# Convert temperature to b as defined in Choudhury et al. (2001) [C2001],
+# equation 31, for the nth sightline
 def bs(n):
 	return np.sqrt(2.0 * k_B * Tss[:, n] / m_p)
 
@@ -67,41 +71,74 @@ def als(n):
 
 # The integrand as in [C2001] equation 30 except with a change of variables to
 # be an integral over z, for the nth sightline
-def integrand1s(n, z0):
+def integrand1s(n, z0, f_scale):
 	prefactor = c * I_al / sqrt_pi
 	voigtFn = voigt(als(n), vArg2s(n, z0))
-	return prefactor * dxs * voigtFn * nHIss[:, n] / (bs(n) * (1.0 + zs))
+	return prefactor * dxs * voigtFn * nHIss[:, n] * f_scale / (bs(n) * (1.0 + zs))
 
 # Optical depth of the nth sightline from the farthest redshift up to z0, for
 # the nth sightline; we integrate using Simpson's rule over all the points that
 # fall in the region and assume the redshifts are in increasing order
-def opticalDepth(n, z0):
-	return si.simps(integrand1s(n, z0), zs)
+def opticalDepth(n, z0, f_scale):
+	return si.simps(integrand1s(n, z0, f_scale), zs)
 
-def output1s(n):
-	return np.array([opticalDepth(n, z0) for z0 in zs])
+def output1s(n, f_scale):
+	return np.array([opticalDepth(n, z0, f_scale) for z0 in zs])
 
 # Attenuation coefficient
-def output2s(n):
-	return np.exp(-output1s(n))
+def output2s(n, f_scale):
+	return np.exp(-output1s(n, f_scale))
 
 # --------------
 # -- Plotting --
 # --------------
 
-def plot(n):
+depthName = "$\\tau_\\mathrm{O\\,I}$"
+fluxName = "$F=e^{-" + depthName[1 : len(depthName) - 1] + "}$"
+
+# Optical depth and flux
+def plot1(n, f_scale):
 	plt.subplot(211)
 	plt.title(f"Optical depth for sightline {n + 1}")
-	plt.semilogy(zs, output1s(n))
-	plt.ylabel("$\\tau_{HI}$")
+	plt.semilogy(zs, output1s(n, f_scale))
+	plt.ylabel(depthName)
 	plt.subplot(212)
-	plt.plot(zs, output2s(n))
-	plt.ylabel("$F=e^{-\\tau_{HI}}$")
+	plt.plot(zs, output2s(n, f_scale))
 	plt.xlabel("$z$")
+	plt.ylabel(fluxName)
+	plt.show()
+
+# A single line for plot2
+def plot2single(n, color, f_scale):
+	plt.plot(zs, output2s(n, f_scale), color = color)
+	powerstring = str(np.log10(f_scale))
+	powerstring = powerstring[: len(powerstring) - 2]
+	return ml.Line2D([], [], color = color, label = f"$f_{{scale}} = 10^{{{powerstring}}}$")
+
+# Varying hydrogen to oxygen fraction
+def plot2(n):
+	plt.subplot(211)
+	plt.title(f"Observed flux for different oxygen to hydrogen ratios $f_{{scale}}$ for sightline ${n + 1}$")
+	count = 3
+	colors = ["r", "g", "b"]
+	f_scales = [1000.0, 100.0, 10.0]
+	lines = [None] * count
+	for i in range(0, count):
+		lines[i] = plot2single(n, colors[i], f_scales[i])
+	plt.ylabel(fluxName)
+	plt.legend(handles = lines, loc = "center right", framealpha = 0.95)
+	plt.subplot(212)
+	colors = ["k", "c", "m"]
+	f_scales = [1.0, 0.1, 0.01]
+	for i in range(0, count):
+		lines[i] = plot2single(n, colors[i], f_scales[i])
+	plt.xlabel("$z$")
+	plt.ylabel(fluxName)
+	plt.legend(handles = lines, loc = "center right", framealpha = 0.95)
 	plt.show()
 
 # Main
 n = 0
 if len(sys.argv) > 0:
 	n = int(sys.argv[1]) - 1
-plot(n)
+plot2(n)
