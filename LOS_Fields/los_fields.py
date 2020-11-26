70f2226e757ab3cf0b33b1200e1b79cfdb6ad091
Daniel B <db737@tutanota.com>
2020-11-26 02:15:23 +0000

    smallfix3

--- LOS_Fields/los_fields.py
+++ LOS_Fields/los_fields.py
@@ -5,8 +5,9 @@ import matplotlib.pyplot as plt
 import matplotlib.lines as ml
 import math
 import sys
+import os
 
-sys.path.append(os.path.join(os.path.dirname(__file__), "../../read_spec_ewald_script.py"))
+sys.path.append(os.path.join(os.path.dirname(__file__), "..", "..", "read_spec_ewald_script.py"))
 
 from numpy import pi
 from read_spec_ewald_script import spectra
