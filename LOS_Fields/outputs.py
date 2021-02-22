6ec2849fedb215fb8d45b2eb3ba838ca3f42affe
Daniel B <db737@tutanota.com>
2021-02-22 02:53:14 +0000

    arg typo

--- LOS_Fields/outputs.py
+++ LOS_Fields/outputs.py
@@ -191,7 +191,7 @@ def test7(n):
 	plt.scatter(zs[mins], flux_data[mins], c = 'r')
 	plt.scatter(zs[maxes], flux_data[maxes], c = 'g')
 	ews = equiv_widths(n, False) * nu_12_OI * 1.0e-10 / c
-	plt.errbar(zs[mins], flux_data[mins], xErr = ews / 2, 'k')
+	plt.errbar(zs[mins], flux_data[mins], xErr = ews / 2, fmt = 'k')
 	plt.show()
 
 # Check inputs are as expected
