c5f7fbb0a238b02f0a09dd545312034482cd7c33
Daniel B <db737@tutanota.com>
2021-02-22 04:28:02 +0000

    typo

--- LOS_Fields/outputs.py
+++ LOS_Fields/outputs.py
@@ -189,7 +189,7 @@ def test7(n):
 	maxes = extrema(n, False, True, False)
 	plt.scatter(zs[mins], flux_data[mins], c = 'r')
 	plt.scatter(zs[maxes], flux_data[maxes], c = 'g')
-	ews = equiv_widths(n, False) * nu_12_OI * 1.0e-10 / c
+	ews = equiv_widths(n, True) * nu_12_OI * 1.0e-10 / c
 	print(ews)
 	plt.errorbar(zs[mins], flux_data[mins], xerr = ews / 2, fmt = 'none', capsize = 10.0)
 	plt.show()
