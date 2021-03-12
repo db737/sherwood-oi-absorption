69d63fd2ca4dc299b99fd14461aa6910ae103f69
Daniel B <db737@tutanota.com>
2021-03-12 13:55:49 +0000

    try local import

--- LOS_Fields/outputs.py
+++ LOS_Fields/outputs.py
@@ -341,9 +341,10 @@ def example1(n):
 
 # Fake trough example
 def example2(n):
+	from numpy.random import default_rng
 	xs = np.linspace(-1.0, 1.0, 200)
 	ys = 1.0 - 0.3 * np.exp(-0.5 * (xs / 0.4) ** 2.0)
-	rng = np.random.default_rng()
+	rng = default_rng()
 	noise = 0.01 * rng.normal(0.0, 1.0, 200)
 	plt.plot(xs, ys + noise)
 	
