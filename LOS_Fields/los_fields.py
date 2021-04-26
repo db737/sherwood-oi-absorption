1494878ebf230f60ab93893a5de1d07aecb7e41a
Daniel B <db737@tutanota.com>
2021-04-26 03:42:53 +0100

    typo

--- LOS_Fields/los_fields.py
+++ LOS_Fields/los_fields.py
@@ -229,8 +229,8 @@ def integrand1s(n, z0, hydrogen, ssOnly):
 def expanded(xss):
 	left = xss[count - extra : count, :]
 	right = xss[0 : extra, :]
-	x2ss = np.append(left, xss)
-	x2ss = np.append(x2ss, right)
+	x2ss = np.append(left, xss, axis = 0)
+	x2ss = np.append(x2ss, right, axis = 0)
 	return x2ss
 
 # Optical depth of the nth sightline from the farthest redshift up to z0, for
